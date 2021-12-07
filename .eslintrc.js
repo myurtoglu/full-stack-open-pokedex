0354ac6be0000cd01521221a86becbe92c368f4f
Mete Yurtoglu <myurtoglu@google.com>
2021-12-07 14:48:09 -0800

    Fix lint issues.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   "env": {
-    "browser": true,
+    "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "browser": true,
   },
   "extends": [
     "eslint:recommended",
@@ -43,7 +44,12 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 }
